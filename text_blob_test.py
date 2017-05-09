4832cfdf404771cf752200c3c9be5b0c94a8961c
Austin Wood <austin@austinkwood.com>
2017-05-09 11:28:21 -0700

    Add NLTK download script

--- text_blob_test.py
+++ text_blob_test.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
-
+import nltk
+nltk.data.path.append("/Users/austinwood/Code/JournalData/nltk_data")
 
 # str = open('./corpus/entry1.txt', 'r').read()
 str = "Hi my name is Claire and I really hope this works."
