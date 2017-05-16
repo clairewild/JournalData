e3f4ce016e7e3ec3c89f2ec7987ac1f14ea57d97
Austin Wood <austin@austinkwood.com>
2017-05-16 10:36:05 -0700

    Generate and render word cloud for arbitrary data

---
+++ frontend/components/cloud.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from 'react-dom';
+import WordCloud from 'react-d3-cloud';
+
+class Cloud extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const data = [
+      { text: 'Hey', value: 1000 },
+      { text: 'lol', value: 200 },
+      { text: 'first impression', value: 800 },
+      { text: 'very cool', value: 1000000 },
+      { text: 'duck', value: 10 },
+    ];
+
+    const fontSizeMapper = word => Math.log2(word.value) * 5;
+    const rotate = () => {
+      return (Math.random() >= 0.5) ? 0 : 90;
+    }
+
+    return (
+      <WordCloud
+        data={data}
+        fontSizeMapper={fontSizeMapper}
+        rotate={rotate}
+      />
+    );
+  }
+}
+
+export default Cloud;
