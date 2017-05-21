7e8cc4cb2711ff3da506b6459812c5c125b2130b
Claire Wild <claire.wild15@gmail.com>
2017-05-21 13:21:36 -0700

    Fix merge conflict

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 import TimeOrientationContainer from './time_orientation/time_orientation_container';
 import CloudContainer from './word_cloud/cloud_container';
-<<<<<<< HEAD
 import PronounsContainer from './pronouns/pronouns_container';
-=======
->>>>>>> 7d79913f12ff3a29073879a93012a7d1a49db44c
+
 
 class Home extends React.Component {
   constructor(props) {
@@ -17,14 +15,10 @@ class Home extends React.Component {
   }
 
   render() {
-    const test = {backgroundColor: 'blue'};
+    const test = {backgroundColor: "blue"};
     return (
-<<<<<<< HEAD
-      <div>
-=======
-      <div id='home'>
+      <div id="home">
         <CloudContainer />
->>>>>>> 7d79913f12ff3a29073879a93012a7d1a49db44c
         <TimeOrientationContainer />
         <CloudContainer />
         <PronounsContainer />
