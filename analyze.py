479ead7e93049a5f5959f30ca4dfe1b3a27f00d9
Austin Wood <austin@austinkwood.com>
2017-05-22 11:46:19 -0700

    Fix changes to backend

--- analyze.py
+++ analyze.py
@@ -5,7 +5,7 @@ import time
 from time_orientation import time_orientation_fn
 from word_cloud import word_cloud_fn
 from pronouns import pronouns_fn
-from backend.word_count import word_count_fn
+from word_count import word_count_fn
 
 def analyze_json():
 
