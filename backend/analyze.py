4b4013988ccb7d96c08aa92d0b7709956d4bc358
Austin Wood <austin@austinkwood.com>
2017-05-22 12:01:29 -0700

    Move NLP files to backend directory

--- backend/analyze.py
+++ backend/analyze.py
@@ -2,10 +2,10 @@ import json
 from pprint import pprint
 import time
 
-from time_orientation import time_orientation_fn
-from word_cloud import word_cloud_fn
-from pronouns import pronouns_fn
-from word_count import word_count_fn
+from backend.time_orientation import time_orientation_fn
+from backend.word_cloud import word_cloud_fn
+from backend.pronouns import pronouns_fn
+from backend.word_count import word_count_fn
 
 def analyze_json():
 
